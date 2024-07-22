0c15143473633848c458d8c2de830f874ad2b409
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-07-22 12:53:10 +0530

    chore: express js setup

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res)=>{
+    res.send("Hi")
+})
+
+module.exports = app
